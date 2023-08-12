6b9cb1423cc38417da1099873690c717a86632ef
lovetocode <dshaggy28@yahoo.com>
2023-08-11 17:01:46 -0700

    Update main.cob

--- main.cob
+++ main.cob
@@ -1,1 +1,9 @@
+IDENTIFICATION DIVISION.
+       PROGRAM-ID. "GuessNumber".
+       AUTHOR.     "Daniel Thornton".
+     *This program is a guess the number game.
+       ENVIRONMENT DIVISION.
 
+       INPUT-OUTPUT SECTION.
+
+       DATA DIVISION

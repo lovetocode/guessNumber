f742d27caae3219806297917ee517f0ecb14a518
lovetocode <dshaggy28@yahoo.com>
2023-08-12 00:38:15 +0000

    added author

--- main.cob
+++ main.cob
@@ -1,7 +1,15 @@
-000100 IDENTIFICATION DIVISION.
-000200 PROGRAM-ID. HELLO-WORLD.
-000300
-000400 PROCEDURE DIVISION.
-000500   DISPLAY 'Hello, World!'.
-000600 STOP RUN.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "guessNumber".
+       AUTHOR "Daniel Thornton".
+      *This is a guess the number game
+
+       ENVIRONMENT DIVISION.
+
+       INPUT-OUTPUT SECTION.
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+         DISPLAY 'Hello, World!'.
+       STOP RUN.
 

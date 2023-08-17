4d380afca255ca05f21ca93627da2ed6c8db8155
lovetocode <dshaggy28@yahoo.com>
2023-08-17 00:35:25 +0000

    added quit-game

--- main.cob
+++ main.cob
@@ -13,31 +13,55 @@
       *-------------------------
 
        WORKING-STORAGE SECTION.
-       01 WS-GUESSES-TAKEN PIC X(9).
-       01 WS-USER-GUESS PIC X(5).
-       01 WS-SEED PIC 9(08) value 1.
+       01 WS-NUMBER-GUESSES PIC 999.
+       01 WS-USER-GUESS PIC 9999.
+       01 WS-SEED PIC 9 VALUE 1.
        01 WS-RAND PIC ZZZ9.
        01 WS-RUN PIC 9 VALUE 1.
-       78 WS-NEWLINE VALUE x"0d".
+       01 WS-NEWLINE PIC 9(1) value 1. 
+       01 WS-PLAY-AGAIN PIC X(5) VALUE "y".
        
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-             COMPUTE WS-RAND = FUNCTION RANDOM(WS-SEED) * 10 + 1.
+       PERFORM GAME-LOOP UNTIL WS-RUN = 0.
+       STOP RUN.
+             
+       GAME-LOOP.
+           EVALUATE WS-PLAY-AGAIN
+           WHEN "n"
+             PERFORM QUIT-GAME
+           WHEN OTHER
+             COMPUTE WS-RAND = FUNCTION RANDOM(WS-SEED) * 10000 + 1.
              MOVE 1 TO WS-RUN.
-             DISPLAY "Guess the number !".
+             DISPLAY "Welcome to guess the number...... version 1.0."
+             DISPLAY "Guess a number between 1 and who knows ". 
              ACCEPT WS-USER-GUESS.
              IF WS-RAND = WS-USER-GUESS THEN
-                DISPLAY "You won!"
-             ELSE IF WS-RAND < WS-USER-GUESS THEN
-                DISPLAY "Your guess is to high"
+                DISPLAY "You won! "
+                PERFORM QUIT-GAME
              ELSE IF WS-RAND > WS-USER-GUESS THEN
-                DISPLAY "You guess is to low"
-             ELSE 
-                DISPLAY "Your guess is invalid".
-              
-             
-            
-  
-       STOP RUN.
+                  DISPLAY "Your guess is to low"
+              ELSE 
+                  DISPLAY "Your guess is to high"
+        STOP RUN.
+
+       CALCULATE-NUM-GUESSES.
+          COMPUTE WS-NUMBER-GUESSES = WS-NUMBER-GUESSES + 1.
+
+       QUIT-GAME.
+        DISPLAY "Do you want to play again? "
+          ACCEPT WS-PLAY-AGAIN
+          MOVE FUNCTION LOWER-CASE(WS-PLAY-AGAIN) TO WS-PLAY-AGAIN.
+          IF WS-PLAY-AGAIN = "y"
+              PERFORM MAIN-PROCEDURE
+          ELSE IF WS-PLAY-AGAIN = "n"
+              DISPLAY "Goodbye for now...!"
+             STOP RUN
+      
+          STOP RUN.
+
+         
+
+      
 
